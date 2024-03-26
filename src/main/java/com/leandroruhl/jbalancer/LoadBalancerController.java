0fd29926e9e7b9597abd8488bead097d92f1e102
LeandroFernandezRuhl <leandro.feruhl@gmail.com>
2024-03-26 19:10:28 -0300

    Implement basic load balancing functionality in LoadBalancerController
    This commit introduces changes to the LoadBalancerController class to implement basic load balancing functionality. It includes the following changes:
    
    * Add fields webClient, serverUrls, and requestCounter to manage the web client and maintain a list of backend server URLs.
    * Update the constructor to initialize the web client, request counter, and server URLs list.
    * Modify the forwardRequest method to forward incoming requests to the backend servers based on a round-robin algorithm.
    * Incorporate error handling to handle WebClientResponseExceptions and return appropriate error messages for client errors.

--- src/main/java/com/leandroruhl/jbalancer/LoadBalancerController.java
+++ src/main/java/com/leandroruhl/jbalancer/LoadBalancerController.java
@@ -1,18 +1,62 @@
 package com.leandroruhl.jbalancer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+
 @RestController
-@RequestMapping("")
 @Slf4j
 public class LoadBalancerController {
-    @GetMapping()
-    public Mono<String> get() {
-        log.info("Hello from Load Balancer!");
-        return Mono.just("Hello from Load Balancer!");
+    private final WebClient webClient;
+    private ArrayList<String> serverUrls;
+    private Integer requestCounter;
+
+    public LoadBalancerController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+        this.requestCounter = 0;
+        this.serverUrls = new ArrayList<>();
+        serverUrls.add("http://localhost:8081");
+        serverUrls.add("http://localhost:8082");
+    }
+
+    @RequestMapping(value = "/**")
+    public Mono<String> forwardRequest(ServerHttpRequest request) {
+        String backendServerUrl = determineBackendServerUrl();
+        this.requestCounter++;
+
+        String uri = UriComponentsBuilder.fromHttpUrl(backendServerUrl)
+                .path(request.getPath().value())
+                .query(request.getURI().getQuery())
+                .build()
+                .toUriString();
+
+        return webClient.method(request.getMethod())
+                .uri(uri)
+                .headers(headers -> headers.putAll(request.getHeaders()))
+                .body(BodyInserters.fromDataBuffers(request.getBody()))
+                .retrieve()
+                .bodyToMono(String.class)
+                .onErrorResume(WebClientResponseException.class, ex -> {
+                    if (ex.getStatusCode().is4xxClientError()) {
+                        return Mono.just("Error: " + ex.getStatusCode().value() + " " + ex.getStatusText());
+                    } else {
+                        return Mono.error(ex);
+                    }
+                });
+    }
+
+
+    private String determineBackendServerUrl() {
+        return serverUrls.get(requestCounter % serverUrls.size());
     }
 }
